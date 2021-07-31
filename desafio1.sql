9171bbdf70a80c12de1061b30c27dcb1fb659407
lucas <lucasportellaagu@gmail.com>
2021-07-31 18:53:47 -0300

    fix sintax erros

--- desafio1.sql
+++ desafio1.sql
@@ -8,13 +8,13 @@ CREATE TABLE plans(
     plan_id INT PRIMARY KEY AUTO_INCREMENT,
     plan VARCHAR(30) NOT NULL,
     plan_value DECIMAL(4,2) NOT NULL
-)
+);
 
 INSERT INTO plans(plan, plan_value)
 VALUES
 ('gratuito', 0),
-('familiar', 7,99),
-('universitário', 5,99)
+('familiar', 7.99),
+('universitário', 5.99);
 
 CREATE TABLE users(
     user_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -22,49 +22,49 @@ CREATE TABLE users(
     age INT NOT NULL,
     plan_id INT NOT NULL,
     FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
-)
+);
 
 INSERT INTO users(user, age, plan_id)
 VALUES
 ('Thati', 23, 1),
 ('Cintia', 35, 2),
 ('Bill', 20, 3),
-('Roger', 45, 1)
+('Roger', 45, 1);
 
 
-CREATE TABLE artists(
+CREATE TABLE artists (
     artist_id INT PRIMARY KEY AUTO_INCREMENT,
-    artist VARCHAR(60) NOT NULL,
-)
+    artist VARCHAR(60) NOT NULL
+);
 
 INSERT INTO artists(artist)
 VALUES
 ('Walter Phoenix'),
 ('Peter Strong'),
 ('Lance Day'),
-('Freedie Shannon')
+('Freedie Shannon');
 
 CREATE TABLE albums(
     album_id INT PRIMARY KEY AUTO_INCREMENT,
     album VARCHAR(60) NOT NULL,
     artist_id INT NOT NULL,
     FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
-)
+);
 
-INSERT INTO albums(album, album, artist_id)
+INSERT INTO albums(album, artist_id)
 VALUES
 ('Envious', 1),
 ('Exuberant', 1),
-('Hallowed Steam', 2)
-('Incandescent', 3)
-('Temporary Culture', 4)
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
 CREATE TABLE songs(
     song_id INT PRIMARY KEY AUTO_INCREMENT,
     song VARCHAR(60) NOT NULL,
     album_id INT NOT NULL,
     FOREIGN KEY (album_id) REFERENCES albums(album_id)
-)
+);
 
 INSERT INTO songs(song, album_id)
 VALUES
@@ -85,15 +85,15 @@ VALUES
 ("Honey, Let's Be Silly", 4),
 ('Thang Of Thunder', 5),
 ('Words Of Her Life', 5),
-('Without My Streets', 5)
+('Without My Streets', 5);
 
 CREATE TABLE following(
     user_id INT NOT NULL,
     artist_id INT NOT NULL,
-    PRIMARY KEY (user_id, artist_id)
+    PRIMARY KEY (user_id, artist_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
-)
+);
 
 INSERT INTO following(user_id, artist_id)
 VALUES
@@ -104,16 +104,18 @@ VALUES
 (2, 3),
 (3, 1),
 (3, 2),
-(4, 4)
+(4, 4);
 
 
-CREATE TABLE historic(
+CREATE TABLE historic (
     user_id INT NOT NULL,
     song_id INT NOT NULL,
-    PRIMARY KEY (user_id, song_id)
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-    FOREIGN KEY (song_id) REFERENCES songs(song_id)
-)
+    PRIMARY KEY (user_id , song_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id),
+    FOREIGN KEY (song_id)
+        REFERENCES songs (song_id)
+);
 
 INSERT INTO historic(user_id, song_id)
 VALUES
@@ -130,4 +132,4 @@ VALUES
 (3, 6),
 (4, 3),
 (4, 18),
-(4, 11)
+(4, 11);

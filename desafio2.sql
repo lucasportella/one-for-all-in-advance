e8b7f04ce75253f46d618ee0cc1f1c81e9bc0856
lucas <lucasportellaagu@gmail.com>
2021-08-02 10:53:00 -0300

    fix lint

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,11 @@
 CREATE VIEW estatisticas_musicais AS
-SELECT
-COUNT(canc.song_id) AS 'cancoes',
-COUNT(DISTINCT art.artist_id) AS 'artistas',
-COUNT(DISTINCT alb.album_id) AS 'albuns'
-FROM SpotifyClone.songs as canc
-INNER JOIN SpotifyClone.albums as alb ON canc.album_id = alb.album_id
-INNER JOIN SpotifyClone.artists as art ON alb.artist_id = art.artist_id;
+    SELECT 
+        COUNT(canc.song_id) AS 'cancoes',
+        COUNT(DISTINCT art.artist_id) AS 'artistas',
+        COUNT(DISTINCT alb.album_id) AS 'albuns'
+    FROM
+        SpotifyClone.songs AS canc
+            INNER JOIN
+        SpotifyClone.albums AS alb ON canc.album_id = alb.album_id
+            INNER JOIN
+        SpotifyClone.artists AS art ON alb.artist_id = art.artist_id;
